96b0176db766a9ca77132f0d751e9289f090f2a6
Andrew Gray <Andrew.rhysgray@hotmail.co.uk>
2020-02-25 16:25:34 +0000

    getters and setters created

--- src/main/java/com/example/relationship_homework_java/RelationshipHomework/Models/File.java
+++ src/main/java/com/example/relationship_homework_java/RelationshipHomework/Models/File.java
@@ -1,4 +1,12 @@
 package com.example.relationship_homework_java.RelationshipHomework.Models;
 
 public class File {
+
+//    private String name;
+//
+//    private String extension;
+//
+//    private String size;
+//
+//    private String folder;
 }
