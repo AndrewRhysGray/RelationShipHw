2c2f544f7ca5f4bc99311412a4697bd9f1e3f2f0
Andrew Gray <Andrew.rhysgray@hotmail.co.uk>
2020-02-26 09:57:00 +0000

    done

--- src/main/java/com/example/relationship_homework_java/RelationshipHomework/Models/Folder.java
+++ src/main/java/com/example/relationship_homework_java/RelationshipHomework/Models/Folder.java
@@ -1,4 +1,88 @@
 package com.example.relationship_homework_java.RelationshipHomework.Models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name = "folders" )
 public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @JsonIgnoreProperties("folders")
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false )
+    private User user;
+
+    @JsonIgnoreProperties(value = "folders")
+    @OneToMany
+    @JoinTable(
+            name = "files_in_folders",
+            joinColumns = { @JoinColumn(
+                    name = "folder_id",
+                    nullable = false,
+                    updatable = false)
+    )
+
+
 }
+
+
+    private List<ArrayList> files;
+
+
+    public Folder(Long id, String title, User user) {
+
+
+    this.title = title;
+    this.user = user;
+    this.files = new ArrayList<>();
+    }
+
+    public Folder() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<ArrayList> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<ArrayList> files) {
+        this.files = files;
+    }
+}
+
+
--- src/main/java/com/example/relationship_homework_java/RelationshipHomework/Models/User.java
+++ src/main/java/com/example/relationship_homework_java/RelationshipHomework/Models/User.java
@@ -1,14 +1,62 @@
 package com.example.relationship_homework_java.RelationshipHomework.Models;
 
 
-import com.example.relationship_homework_java.RelationshipHomework.RelationshipHomeworkApplication;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@SpringBootApplication
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+@Entity
+@Table(name = "name")
 public class User {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RelationshipHomeworkApplication.class, args);
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+@JsonIgnoreProperties("user")
+@OneToMany(mappedBy = "user")
+    private List<Folder> folders;
+
+
+    public  User(String name) {
+
+        this.name = name;
+        this.folders = new ArrayList<>();
+
+
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Folder> getFolders() {
+        return folders;
+    }
+
+    public void setFolders(List<Folder> folders) {
+        this.folders = folders;
     }
 }
